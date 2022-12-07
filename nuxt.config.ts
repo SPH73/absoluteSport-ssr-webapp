22eb670ae57a4645fa30069f37cbe075caffca76
Sue Holder <susanpholder@gmail.com>
2022-12-07 16:47:37 +0000

    add css and nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  ssr: true,
+  modules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config',
+    exposeConfig: true,
+  },
+  css: [
+    '~/assets/css/global.css'
+  ],
+  runtimeConfig: {
+    private: {
+      atApiKey: process.env.AT_API_KEY,
+    },
+    public: {
+      atBaseId: process.env.AT_BASE_ID
+    }
+  }
 })
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  'components/**/*.{vue,js,ts}',
+  'layouts/**/*.vue',
+  'pages/**/*.vue',
+  'composables/**/*.{js,ts}',
+  'plugins/**/*.{js,ts}',
+  'App.{js,ts,vue}',
+  'app.{js,ts,vue}',
+  'Error.{js,ts,vue}',
+  'error.{js,ts,vue}',
+  'content/**/*.md',
+],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary': 'rgb(var(--color-primary) / <alpha-value>)',
+      'secondary': 'rgb(var(--color-secondary) / <alpha-value>)',
+      'accent': 'rgb(var(--color-accent) / <alpha-value>)',
+      'dark': 'rgb(var(--color-dark) / <alpha-value>)',
+      'light': 'rgb(var(--color-light) / <alpha-value>)',
+      'backdrop': 'rgb(var(--color-backdrop) / <alpha-value>)',
+    },
+    screens: {
+      'xs': '320px', //min-width
+      'sm': '480px',
+      'md': '768px',
+      'lg': '976px',
+      'xl': '1440px',
+      '2xl':'1600px',
+      '3xl':'1920px',
+    },
+    fontFamily: {
+      'play': ['Play-Bold','sans-serif'],
+      'mulish': ['Mulish-Semibold','sans-serif'],
+      'inter-regular': ['Inter-Regular','sans-serif'],
+      'inter-medium': ['Inter-Medium','sans-serif'],
+      'inter-bold': ['Inter-Bold','sans-serif'],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms')
+  ],
+};
