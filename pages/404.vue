e330843878d938cab9f5756941d77d7cb44c0515
Sue Holder <susanpholder@gmail.com>
2022-12-10 09:34:39 +0000

    added basic 404 error page

---
+++ pages/404.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>404</h1>
+    <p>Oops that page doesn't exist or has been moved</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
