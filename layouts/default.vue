a1c8e88389930d1635896bd8293bb67bba34ad67
Sue Holder <susanpholder@gmail.com>
2022-12-10 09:33:07 +0000

    updated default layout styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,17 +4,35 @@
       This is the ticker notification bar
     </div>
     <header class="bg-accent max-h-fit w-full flex items-center justify-between p-2">
-      <NuxtLink to="/">
+      <NuxtLink class="mr-4" to="/">
         <img src="/img/logo.webp" alt="Logo" />
       </NuxtLink>
-      <nav class="text-secondary flex justify-evenly text-lg uppercase font-inter-medium">
-        <NuxtLink to="/" class="block mt-4 lg:inline-block lg:mt-0 mr-4">Home </NuxtLink>
-        <NuxtLink class="block mt-4 lg:inline-block lg:mt-0 mr-4">Contact </NuxtLink>
-        <NuxtLink class="block mt-4 lg:inline-block lg:mt-0 mr-4">School Clubs </NuxtLink>
-        <NuxtLink class="block mt-4 lg:inline-block lg:mt-0 mr-4"
-          >Activity Camps
+      <nav class="flex justify-evenly uppercase font-inter-medium">
+        <NuxtLink
+          to="/"
+          class="text-secondary active:text-primary block mt-4 lg:inline-block lg:mt-0 mr-4"
+        >
+          Home
         </NuxtLink>
-        <NuxtLink to="/parties" class="block mt-4 lg:inline-block lg:mt-0 mr-4">
+        <NuxtLink
+          class="text-secondary active:text-primary block mt-4 lg:inline-block lg:mt-0 mr-4"
+        >
+          Contact
+        </NuxtLink>
+        <NuxtLink
+          class="text-secondary active:text-primary block mt-4 lg:inline-block lg:mt-0 mr-4"
+        >
+          School Clubs
+        </NuxtLink>
+        <NuxtLink
+          class="text-secondary active:text-primary block mt-4 lg:inline-block lg:mt-0 mr-4"
+        >
+          Activity Camps
+        </NuxtLink>
+        <NuxtLink
+          to="/parties"
+          class="text-secondary active:text-primary block mt-4 lg:inline-block lg:mt-0 mr-4"
+        >
           Kids Parties
         </NuxtLink>
       </nav>
@@ -23,7 +41,9 @@
       <slot />
     </div>
     <footer class="bg-dark text-light">
-      <h1>Footer</h1>
+      <div class="container">
+        <h1>Footer</h1>
+      </div>
     </footer>
   </div>
 </template>
