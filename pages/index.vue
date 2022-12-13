a743a47047d4a0f5976bdc8a2ca81066e8605ef4
Sue Holder <susanpholder@gmail.com>
2022-12-13 19:46:25 +0000

    added parallax for lg screen background imgs

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
 <script setup></script>
 <template>
   <section
-    class="bg-blend-overlay bg-neutral-600 bg-[url('/img/hero_1920.webp')] bg-center bg-no-repeat bg-cover w-full h-[90vh] md:h-[70vh] pt-10 pb-4"
-  >
+           class="bg-blend-overlay lg:bg-fixed bg-neutral-600 bg-[url('/img/hero_1920.webp')] bg-center bg-no-repeat bg-cover w-full h-[90vh] md:h-[70vh] pt-10 pb-4">
     <div class="max-w-6xl mx-auto">
       <div class="container relative font-play">
         <h1>Fun, Laughter, Games and Exercise with AbsoluteSport</h1>
@@ -21,47 +20,45 @@
         <BaseContent>
           <template #title>During School Term's</template>
           <template #sub-1>Morning Clubs</template>
-          <template #para-1
-            >Our Morning Clubs at West Wittering and Sidlesham Primary Schools are
-            something that all the children will love. We include many fun sports like
-            Fencing, Archery and Table Tennis.</template
-          >
+          <template #para-1>Our Morning Clubs at West Wittering and Sidlesham
+            Primary Schools are
+            something that all the children will love. We include many fun
+            sports like
+            Fencing, Archery and Table Tennis.</template>
           <template #sub-2>Afternoon Clubs</template>
-          <template #para-2
-            >After School Clubs give the children that opportunity to learn a new sport or
-            enhance their current skills in their favourite sports. We do a wide range of
-            clubs with KS1 and KS2 children.</template
-          >
+          <template #para-2>After School Clubs give the children that
+            opportunity to learn a new sport or
+            enhance their current skills in their favourite sports. We do a wide
+            range of
+            clubs with KS1 and KS2 children.</template>
         </BaseContent>
       </BaseCard>
     </div>
   </section>
   <!-- About -->
   <section
-    class="bg-blend-overlay bg-neutral-700 bg-[url('/img/home_about_1920.webp')] lg:bg-center sm:bg-left bg-no-repeat bg-cover w-full h-[90vh] py-10"
-  >
+           class="bg-blend-overlay lg:bg-fixed bg-neutral-700 bg-[url('/img/home_about_1920.webp')] lg:bg-center sm:bg-left bg-no-repeat bg-cover w-full h-[90vh] py-10">
     <div class="max-w-6xl mx-auto">
       <div class="container">
         <BaseContent>
           <template #title>“FUN FOR EVERYONE” - AbsoluteSport</template>
-          <template #sub-1
-            >Service Provider to Parents, Schools, Councils and Sport Coaches in West
-            Sussex and Hampshire</template
-          >
-          <template #para-1
-            >Whether you're a parent seeking ways to help your children become more active
-            or a school or other organisation looking for a provider of first-class sports
-            coaching, mentoring or training, AbsoluteSport is certified to help.</template
-          >
-          <template #para-2
-            >Our highly qualified and experienced team will provide programmes and
-            activities tailored to your exact needs.</template
-          >
-          <template #para-3
-            >Our mission is to engage children and young people in physical activity, by
-            providing innovative delivery and training services and the resources to
-            educate and inspire.</template
-          >
+          <template #sub-1>Service Provider to Parents, Schools, Councils and
+            Sport Coaches in West
+            Sussex and Hampshire</template>
+          <template #para-1>Whether you're a parent seeking ways to help your
+            children become more active
+            or a school or other organisation looking for a provider of
+            first-class sports
+            coaching, mentoring or training, AbsoluteSport is certified to
+            help.</template>
+          <template #para-2>Our highly qualified and experienced team will
+            provide programmes and
+            activities tailored to your exact needs.</template>
+          <template #para-3>Our mission is to engage children and young people
+            in physical activity, by
+            providing innovative delivery and training services and the
+            resources to
+            educate and inspire.</template>
         </BaseContent>
         <!-- <p>Read more about AbsoluteSport -></p> -->
       </div>
