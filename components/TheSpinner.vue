86a528d03a1e60c0c88308074f8af0d3f7ac3c94
Sue Holder <susanpholder@gmail.com>
2023-01-04 17:11:16 +0000

    added loading spinner

---
+++ components/TheSpinner.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div
+    class="flex max-w-md h-20 text-center justify-center items-center backdrop-blur-sm backdrop-grayscale rounded-full p-4 m-auto"
+  >
+    <div
+      class="block w-10 h-10 m-7 m-auto rounded-full border-4 border-accent border-x-primary border-y-secondary motion-safe:animate-spin"
+    ></div>
+    <p class="font-mulish text-light animate-pulse">
+      Fetching&nbsp;
+      <slot name="fetching" />
+    </p>
+  </div>
+</template>
