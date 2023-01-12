00d4b0f9011485382a07d0c1023939bd9517c3d6
Sue Holder <susanpholder@gmail.com>
2023-01-12 18:48:57 +0000

    added campsList api endpoint

---
+++ server/api/camps/campsList.ts
@@ -0,0 +1,28 @@
+import Airtable from "airtable";
+
+export default defineEventHandler(async event => {
+  const { atApiKey } = useRuntimeConfig().private;
+  const { atBaseId } = useRuntimeConfig().public;
+  const base = new Airtable({ apiKey: atApiKey }).base(atBaseId);
+
+  const table = base("campsList");
+
+  const records = await table
+    .select({
+      view: "Grid view",
+      // maxRecords: 2,
+      filterByFormula: "NOT({status} = 'past')",
+      sort: [
+        {
+          field: "ws",
+          direction: "asc",
+        },
+      ],
+    })
+    .firstPage();
+  if (!records) {
+    throw Error("Unable to fetch available parties");
+  }
+
+  return records;
+});
