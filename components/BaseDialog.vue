e83cc170a74d48e472f233f0728413972be25255
Sue Holder <susanpholder@gmail.com>
2023-02-13 21:26:10 +0000

    reduce css

--- components/BaseDialog.vue
+++ components/BaseDialog.vue
@@ -5,8 +5,16 @@ const emit = defineEmits(["close"]);
 
 <template>
   <teleport to="body">
-    <div class="backdrop" v-if="open" @click="$emit('close')"></div>
-    <dialog open v-if="open">
+    <div
+      class="fixed inset-0 bg-neutral-900 bg-opacity-75 overflow-y-auto h-full w-full"
+      v-if="open"
+      @click="$emit('close')"
+    ></div>
+    <dialog
+      open
+      v-if="open"
+      class="modal m-0 p-0 fixed top-1/4 left-[10%] w-[80%] md:w-[40rem] md:left-[calc(50%-20rem)] z-100 overflow-hidden rounded-xl shadow-lg"
+    >
       <header class="bg-primary text-light w-full p-4">
         <slot name="header">
           <h2 class="m-0">{{ title }}</h2>
@@ -23,69 +31,18 @@ const emit = defineEmits(["close"]);
 </template>
 
 <style scoped>
-@keyframes dialog {
-  from {
-    opacity: 0;
-    transform: translateY(-50px) scale(0.8);
-  }
-
-  to {
-    opacity: 1;
-    transform: translateY(0) scale(1);
-  }
-}
-
-/* check .modal enter active ***** */
-.modal-enter-active {
-  animation: dialog 0.8s ease-in;
-}
-
-/* check .modal leave active ***** */
-.modal-leave-active {
-  animation: dialog 0.8s ease-out reverse;
-}
-
-.backdrop {
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  width: 100%;
-  background-color: rgba(0, 0, 0, 0.75);
-  z-index: 10;
-}
-
-dialog {
-  position: fixed;
-  top: 20vh;
-  left: 10%;
-  width: 80%;
-  z-index: 100;
-  border-radius: 12px;
-  border: none;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 0;
-  margin: 0;
-  overflow: hidden;
+.modal {
   animation: modal 0.3s ease-in-out forwards;
 }
-
-@media (min-width: 768px) {
-  dialog {
-    left: calc(50% - 20rem);
-    width: 40rem;
-  }
-}
-
-/* @keyframes modal {
+@keyframes modal {
   from {
     opacity: 0;
-    transform: translateY(-50px) scale(0.9);
+    transform: translateY(-50px) scale(0.75);
   }
 
   to {
     opacity: 1;
     transform: translateY(0) scale(1);
   }
-} */
+}
 </style>
