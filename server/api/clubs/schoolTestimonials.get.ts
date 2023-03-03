0a473ccd69dfc9057114fd40a9629d6350e28171
Sue Holder <susanpholder@gmail.com>
2023-03-03 10:16:47 +0000

    add testimonials endpoint

---
+++ server/api/clubs/schoolTestimonials.get.ts
@@ -0,0 +1,22 @@
+import Airtable from "airtable";
+
+export default defineEventHandler(async () => {
+  const { atApiKey } = useRuntimeConfig().private;
+  const { atBaseId } = useRuntimeConfig().public;
+  const base = new Airtable({ apiKey: atApiKey }).base(atBaseId);
+
+  const table = base("schoolsTestimonials");
+
+  const records = await table
+    .select({
+      filterByFormula: 'NOT(featured = "false")',
+      sort: [{ field: "school", direction: "desc" }],
+    })
+    .firstPage();
+  if (!records) {
+    throw Error("Unable to fetch testimonials");
+  }
+  console.log("airtable schoolTests", records);
+
+  return records;
+});
