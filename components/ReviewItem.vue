0165cce6527c3d04bb5ba577f870772d7f9b6c79
Sue Holder <susanpholder@gmail.com>
2023-03-05 17:37:19 +0000

    minor style change substring length

--- components/ReviewItem.vue
+++ components/ReviewItem.vue
@@ -1,21 +1,21 @@
 <script setup>
 const props = defineProps(["review"]);
 const snippet = computed(() => {
-  return props.review.comment.substring(0, 300) + "...";
+  return props.review.comment.substring(0, 250) + "...";
 });
 </script>
 
 <template>
   <div class="text-light">
-    <div class="">
+    <div class="flex space-x-1">
       <font-awesome-icon
         v-for="star in review.rating"
-        class="text-accent text-xl px-1 py-0"
+        class="text-accent text-xl"
         :icon="['fas', 'star']"
       />
     </div>
     <p class="text-sm text-light tracking-wider max-w-prose">
-      {{ review.comment.length < 300 ? review.comment : snippet }}
+      {{ review.comment.length < 250 ? review.comment : snippet }}
     </p>
 
     <h5 class="text-xl tracking-wider text-accent">
@@ -23,5 +23,3 @@ const snippet = computed(() => {
     </h5>
   </div>
 </template>
-
-<style scoped></style>
