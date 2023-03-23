da4eef85bea8af65d0809e7f145b034080d9f3be
Sue Holder <susanpholder@gmail.com>
2023-03-23 18:45:49 +0000

    accessibility label footer links

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -56,22 +56,46 @@ const year = new Date().getFullYear();
       <div id="links-area" class="flex flex-col pt-2">
         <h5 class="mt-2 mb-4 text-accent">LINKS</h5>
 
-        <NuxtLink :to="{ name: 'clubs' }" class="py-2">School Clubs</NuxtLink>
-        <NuxtLink :to="{ name: 'clubs-booking' }" class="py-2"
+        <NuxtLink
+          :to="{ name: 'clubs' }"
+          class="py-2"
+          aria-label="navigate to school clubs page"
+          >School Clubs</NuxtLink
+        >
+        <NuxtLink
+          :to="{ name: 'clubs-booking' }"
+          class="py-2"
+          aria-label="navigate to clubs booking form"
           >Book School Clubs</NuxtLink
         >
-        <NuxtLink :to="{ name: 'parties' }" class="py-2">Kids Parties</NuxtLink>
-        <NuxtLink :to="{ name: 'parties-quote' }" class="py-2"
+        <NuxtLink
+          :to="{ name: 'parties' }"
+          class="py-2"
+          aria-label="navigate to themed kids parties page"
+          >Kids Parties</NuxtLink
+        >
+        <NuxtLink
+          :to="{ name: 'parties-quote' }"
+          class="py-2"
+          aria-label="navigate to party quote form"
           >Request A Party Quote</NuxtLink
         >
-        <NuxtLink :to="{ name: 'camps' }" class="py-2"
+        <NuxtLink
+          :to="{ name: 'camps' }"
+          class="py-2"
+          aria-label="navigate to holiday activty camps page"
           >Holiday Activity Camps</NuxtLink
         >
-        <NuxtLink :to="{ name: 'camps-booking' }" class="py-2"
+        <NuxtLink
+          :to="{ name: 'camps-booking' }"
+          class="py-2"
+          aria-label="navigate to camp booking form"
           >Book Upcoming Camps</NuxtLink
         >
 
-        <NuxtLink to="/faqs" class="py-2">FAQ's</NuxtLink>
+        <NuxtLink to="/faqs" class="py-2" aria-label="navigate to faq's page"
+          >FAQ's</NuxtLink
+        >
       </div>
 
       <div id="legal-area" class="flex flex-col pt-2">
@@ -81,19 +105,25 @@ const year = new Date().getFullYear();
           target="_blank"
           rel="noopener"
           class="py-2"
+          aria-label="go to our privacy policy"
           >Privacy Policy</NuxtLink
         >
         <NuxtLink
           to="https://drive.google.com/file/d/1QTrpRtfwlY5G-23Oj6donEVr_-3Dnno8/view?usp=share_link"
           class="py-2"
+          aria-label="go to our disclaimer policy"
           >Disclaimer</NuxtLink
         >
         <NuxtLink
           to="https://drive.google.com/file/d/1PuZYI5EWZAS5eQCnE8PcAyAYEK-XZC0x/view?usp=share_link"
           class="py-2"
+          aria-label="go to our term's &amp; conditions"
           >T's &amp; C's</NuxtLink
         >
-        <NuxtLink :to="{ name: 'legal-policies' }" class="py-2"
+        <NuxtLink
+          :to="{ name: 'legal-policies' }"
+          class="py-2"
+          aria-label="navigate to our list of policies &amp; procedures"
           >Policies &amp; Procedures</NuxtLink
         >
       </div>
@@ -103,7 +133,6 @@ const year = new Date().getFullYear();
         id="social-area"
         class="flex flex-row gap-x-4 justify-between items-center mt-8"
       >
-        <!-- ToDO add fb share button -->
         <button class="btn-fb-share">
           <NuxtLink
             target="_blank"
@@ -116,20 +145,12 @@ const year = new Date().getFullYear();
             />Share
           </NuxtLink>
         </button>
-        <!-- <button class="btn-fb-share">
-          <NuxtLink to="#">
-            <font-awesome-icon
-              :icon="['fas', 'thumbs-up']"
-              class="text-light w-8 h-8 mr-2"
-            />
-            Like</NuxtLink
-          >
-        </button> -->
         <div id="social-icons" class="flex flex-row gap-x-4 justify-between">
           <NuxtLink
             to="https://www.instagram.com/absolutesport2211/"
             target="_blank"
             rel="noopener"
+            aria-label="connect with us on instagram"
           >
             <font-awesome-icon
               :icon="['fab', 'instagram']"
@@ -139,6 +160,7 @@ const year = new Date().getFullYear();
             to="https://www.facebook.com/AbsoluteSport.AS"
             target="_blank"
             rel="noopener"
+            aria-label="connect with us on facebook"
           >
             <font-awesome-icon
               :icon="['fab', 'facebook']"
@@ -148,6 +170,7 @@ const year = new Date().getFullYear();
             to="https://www.linkedin.com/in/ben-blakeley-b2600522b/"
             target="_blank"
             rel="noopener"
+            aria-label="connect with us on linkedin"
           >
             <font-awesome-icon
               :icon="['fab', 'linkedin']"
@@ -160,6 +183,7 @@ const year = new Date().getFullYear();
           to="https://designdevelophost.co.uk"
           target="_blank"
           rel="noopener"
+          aria-label="go to our web developer"
           >Website By Design Develop Host
         </NuxtLink>
       </div>
