e810e8d9f7c0065060722b5eb7124e01a24948e5
piDev <48051095+SPH73@users.noreply.github.com>
2023-07-23 12:27:04 +0100

    add filtering for current clubs (#6)

--- pages/clubs/upcoming.vue
+++ pages/clubs/upcoming.vue
@@ -26,6 +26,7 @@ useHead({
 });
 
 const { error: listError, data: list } = await useFetch("/api/clubs/sportList");
+const { error: schoolError, data: schoolList } = await useFetch("/api/clubs/schoolList");
 
 const sportList = ref([]);
 let activity = {};
@@ -43,6 +44,44 @@ list.value.forEach((record, index) => {
   };
   sportList.value.push(activity);
 });
+console.log('SportList', sportList.value)
+
+let school = {};
+const schoolOptions = ref([]);
+schoolList.value.forEach((record, index) => {
+  school = {
+    index: index + 1,
+    id: record.id,
+    schoolName: record.fields.schoolName,
+    schoolRef: record.fields.schoolRef,
+    status: record.fields.status,
+    schoolBadge: record.fields.schoolBadge,
+  };
+  schoolOptions.value.push(school);
+});
+console.log('SchoolList', schoolOptions.value);
+
+const currentClubs = computed(() => {
+  return sportList.value.filter(sport => sport.schoolList === selectedSchool.value);
+});
+
+const nextClubs = computed(() => {
+  return sportList.value.filter(sport => sport.schoolList === selectedSchool.value);
+});
+
+const selectedImage = ref(null);
+const selectedSchool = ref(null);
+
+
+const handleImageClick = (image) => {
+  selectedImage.value = image
+  selectedSchool.value = image.filename.split('.')[0]
+
+};
+watchEffect(() => {
+  selectedSchool.value
+  selectedImage.value
+});
 </script>
 
 <template>
@@ -54,98 +93,104 @@ list.value.forEach((record, index) => {
         </h1>
         <p>
           We coach a variety of sports and activities at our clubs and
-          wraparound care at Sidlesham Primary School, West Wittering Primary
-          School and Portfield Primary Academy. These are updated regularly to
-          keep our clubs current and interesting.
+          wraparound care at West Wittering primary, Portfield Primary Academy, Sidlesham Primary and Yapton Primary. These are updated regularly to keep our clubs current and interesting.
         </p>
-        <button class="btn-primary my-4 w-full md:w-fit">
-          <NuxtLink to="clubs/booking" aria-label="book clubs"
-            >book now</NuxtLink
+        <p>Please select a school badge to view the currently available clubs for your child's school.</p>
+       
+        <div class="grid grid-cols-4 gap-4 my-8">
+            <div v-for="items in schoolOptions" class="flex justify-center">
+              <img v-for="image in items.schoolBadge" :key="image.id" :src="image.url" :alt="image.filename" @click="handleImageClick(image)" class="border-4 border-secondary border-solid cursor-pointer aspect-square block">
+            </div>
+        </div>
+         <button class="btn-primary my-4 w-full md:w-fit">
+            <NuxtLink to="/clubs/booking" aria-label="book clubs"
+              >book now</NuxtLink
+            >
+          </button>
+        <div v-if="selectedImage" class="my-8">
+          <h2 class="font-play capitalize">Currently available Clubs <span v-if="selectedImage">for {{ selectedSchool }} </span></h2>
+          <table
+            v-for="activity in currentClubs"
+            class="table-fixed border-separate border-spacing-2 border border-light text-light w-full text-2xl rounded-md mb-4 whitespace-pre-wrap"
           >
-        </button>
-        <table
-          v-for="activity in sportList"
-          class="table-fixed border-separate border-spacing-2 border border-light text-light w-full text-2xl rounded-md mb-4 whitespace-pre-wrap"
-        >
-          <tbody>
-            <tr>
-              <th
-                class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
-              >
-                Activity
-              </th>
-              <td class="bg-light text-dark border border-light p-4 w-3/5">
-                {{ activity.sportName }}
-              </td>
-            </tr>
-            <tr class="">
-              <th
-                class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
-              >
-                What you can expect
-              </th>
-              <td
-                class="bg-light text-dark border border-light p-4 w-3/5 break-words"
-              >
-                <span v-for="item in activity.intro">{{ item }} &nbsp;</span>
-              </td>
-            </tr>
-            <tr>
-              <th
-                class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
-              >
-                Schools
-              </th>
-              <td class="bg-light text-dark border border-light p-4 w-3/5">
-                <span v-for="item in activity.schoolList"
-                  >{{ item }} &nbsp;</span
+            <tbody>
+              <tr>
+                <th
+                  class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
                 >
-              </td>
-            </tr>
-            <tr>
-              <th
-                class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
-              >
-                Year groups
-              </th>
-              <td class="bg-light text-dark border border-light p-4 w-3/5">
-                <span v-for="item in activity.yearRange"
-                  >{{ item }} &nbsp;</span
+                  Activity
+                </th>
+                <td class="bg-light text-dark border border-light p-4 w-3/5">
+                  {{ activity.sportName }}
+                </td>
+              </tr>
+              <tr class="">
+                <th
+                  class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
                 >
-              </td>
-            </tr>
-            <tr>
-              <th
-                class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
-              >
-                Sessions per booking
-              </th>
-              <td class="bg-light text-dark border border-light p-4 w-3/5">
-                {{ activity.sessions }}
-              </td>
-            </tr>
-            <tr>
-              <th
-                class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
-              >
-                Week commencing
-              </th>
-              <td class="bg-light text-dark border border-light p-4 w-3/5">
-                {{ activity.startDate }}
-              </td>
-            </tr>
-            <tr>
-              <th
-                class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
-              >
-               Ending
-              </th>
-              <td class="bg-light text-dark border border-light p-4 w-3/5">
-                {{ activity.endDate }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                  What you can expect
+                </th>
+                <td
+                  class="bg-light text-dark border border-light p-4 w-3/5 break-words"
+                >
+                  <span v-for="item in activity.intro">{{ item }} &nbsp;</span>
+                </td>
+              </tr>
+              <tr>
+                <th
+                  class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
+                >
+                  School
+                </th>
+                <td class="bg-light text-dark border border-light p-4 w-3/5">
+                  {{ activity.schoolList }}
+                </td>
+              </tr>
+              <tr>
+                <th
+                  class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
+                >
+                  Year groups
+                </th>
+                <td class="bg-light text-dark border border-light p-4 w-3/5">
+                  <span v-for="item in activity.yearRange"
+                    >{{ item }} &nbsp;</span
+                  >
+                </td>
+              </tr>
+              <tr>
+                <th
+                  class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
+                >
+                  Sessions per booking
+                </th>
+                <td class="bg-light text-dark border border-light p-4 w-3/5">
+                  {{ activity.sessions }}
+                </td>
+              </tr>
+              <tr>
+                <th
+                  class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
+                >
+                  Week commencing
+                </th>
+                <td class="bg-light text-dark border border-light p-4 w-3/5">
+                  {{ activity.startDate }}
+                </td>
+              </tr>
+              <tr>
+                <th
+                  class="uppercase bg-secondary text-left text-accent border border-secondary p-4 w-2/5"
+                >
+                Ending
+                </th>
+                <td class="bg-light text-dark border border-light p-4 w-3/5">
+                  {{ activity.endDate }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
