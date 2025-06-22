04746ec99e8a5688e5cbbe287e18aea5bcc18057
piDev (Sue Holder) <sue@designdevelophost.co.uk>
2025-06-22 20:57:22 +0100

    add events page

---
+++ pages/events/index.vue
@@ -0,0 +1,82 @@
+<script setup>
+const year = new Date().getFullYear();
+const route = useRoute();
+useHead({
+  title: `${year} Fun Filled Holiday Activity Camps in West Sussex`,
+  meta: [
+    {
+      name: "description",
+      content:
+        "Get your kids active with our fun and engaging, themed Activity Camps for children aged 5-14 years that will keep your kids laughing this school holiday!",
+    },
+    {
+      property: "og:description",
+      content:
+        "Get your kids active with our fun and engaging, themed Activity Camps to children aged 5-14 that will keep your kids laughing this school holiday!",
+    },
+    {
+      property: "og:type",
+      content: "website",
+    },
+  ],
+  link: [
+    {
+      rel: "canonical",
+      href: "https://www.absolutesport.org/events",
+    },
+  ],
+});
+// posterImage is used in the template to display the event poster
+const posterImage = new URL("/assets/images/ffd-poster.svg", import.meta.url)
+  .href;
+</script>
+
+<template>
+  <div class="my-8">
+    <section class="max-w-7xl mx-auto">
+      <div class="container py-4">
+        <h1 class="font-play capitalize">
+          AbsoluteSport Family Fun Day Charity Event
+        </h1>
+        <p>Join us for a day dedicated to family!</p>
+        <p>
+          At our family fun day, we can spend time together building and
+          renewing connections with our loved ones and within our community.
+        </p>
+        <p>
+          We will have games including a family tug of war or maybe you want to
+          have a go at knocking your sibling off of the duel podium?
+        </p>
+        <p>
+          Would you prefer a quieter activity in the woods or doing some craft
+          activities, we have those too!
+        </p>
+        <p>
+          To top it all off we will have a community barbecue and finish the day
+          with a disco. Bring your family and let's have fun as a community!
+        </p>
+        <button class="btn-primary my-4 w-full md:w-fit">
+          <NuxtLink to="/events/booking" aria-label="book tickets"
+            >book tickets</NuxtLink
+          >
+        </button>
+      </div>
+    </section>
+
+    <section class="max-w-7xl mx-auto">
+      <div class="container py-4">
+        <h2 class="font-play capitalize">Event Details</h2>
+        <div></div>
+        <div
+          class="py-4 flex flex-col items-center justify-center overflow-hidden"
+        >
+          <img
+            :src="posterImage"
+            alt="Charity Event Poster"
+            class="w-full h-auto"
+          />
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
